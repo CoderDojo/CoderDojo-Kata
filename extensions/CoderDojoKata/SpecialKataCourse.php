9cf5fd939393248253660cf42810c36063d2c6b8
Ursula Clarke Everett <ursula@coderdojo.org>
2015-06-26 10:06:47 +0100

    Add new extension files

--- extensions/CoderDojoKata/SpecialKataCourse.php
+++ extensions/CoderDojoKata/SpecialKataCourse.php
@@ -66,7 +66,7 @@ class SpecialKataCourse extends SpecialPage {
 		$this->isVideoEnabled = ($this->namespaceID == 3550) ? true : false;
 	}
 	
-	function execute($subPage = false) {
+	function execute() {
 		$this->out = $this->getOutput();
 		$this->initPage();
 		
---
+++ extensions/CoderDojoKata/SpecialKataSearch.i18n.php
@@ -0,0 +1,9 @@
+<?php
+$messages = array();
+
+$messages['en'] = array(
+	'katamentorstutoriallist' => 'Kata for Mentors - Tutorials List',
+	'katamentorstutoriallist_notavailable' => 'Not Available'
+);
+
+?>
